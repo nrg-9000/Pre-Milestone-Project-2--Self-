a7eda1ee22dc6080e3716bb2489b190b9530c66f
nrg_9000 <nrg.devcode@gmail.com>
2024-07-28 02:02:05 +0530

    Start of the program

---
+++ Project Details.py
@@ -0,0 +1,12 @@
+'''
+Write a program to create the card game WAR
+Two players will play until they give the same value card. Then war of five cards\
+Project will be made in different classes
+Card Class - To create the individual cards
+Deck Class - To create a Deck of cards
+Player Class - To create two players who will be playing with the decks
+Game Class - This wil contain the main logic of the game
+'''
+
+if __name__ == "__main__":
+    print('Please run the Game file!')
